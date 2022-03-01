f433ef8bdfe6f221e02c95c46d047de4e374f857
Juliana Balluffi-Fry <jballuffi@gmail.com>
2022-03-01 09:37:32 -0700

    name rail plot

--- R/03-singlechoice_results.R
+++ R/03-singlechoice_results.R
@@ -59,17 +59,19 @@ source("R/ggplot_themes.R")
 #read in data for diet nutritional rails
 rails <- fread("Output/dietrails.rds")
 
-ggplot()+
+(feedingratesrails<-
+  ggplot()+
   geom_line(aes(x = F1I, y = P1I), color = "black", data = rails)+
   geom_line(aes(x = F2I, y = P2I), color = "black", data = rails)+
   geom_line(aes(x = F3I, y = P3I), color = "black", data = rails)+
   geom_line(aes(x = F4I, y = P4I), color = "black", data = rails)+
-  geom_point(aes(x = Consumed_NDF, y = Consumed_CP, color = Individual), size = 4, data = SCdiets)+
+  geom_point(aes(x = Consumed_NDF, y = Consumed_CP, color = Individual), size = 2.5, data = SCdiets)+
   labs(x="Fibre intake (g/kg/day)", y="Protein intake (g/kg/day)")+
-  themerails
+  themerails)
 
 
 
 #save plots
 ggsave("Output/consumptionbarplot.jpeg", ConsumptionRates, width = 4, height = 3, unit = "in")
 ggsave("Output/weightboxplot.jpeg", WeightChange, width = 4, height = 3, unit = "in")
+ggsave("Output/singlechoicerails.jpeg", feedingratesrails, width = 5, height = 3, unit = "in")
