e0214a1509931d01585485ffef5b4c09a6342326
Juliana Balluffi-Fry <jballuffi@gmail.com>
2022-02-28 18:12:33 -0700

    remove theme now that it's in its own script

--- R/02-multichoice_results.R
+++ R/02-multichoice_results.R
@@ -23,18 +23,7 @@ MCtotals <- MCdiets[, .(sum(Consumed_CP), sum(Consumed_NDF)), by = ID]
 names(MCtotals) <- c("ID", "CP", "NDF")
 
 
-#create theme for ggplots
-themerails<-theme(axis.title = element_text(size=13),
-                  axis.text = element_text(size=10),
-                  axis.line.x.top = element_blank(),
-                  axis.line.y.right = element_blank(),
-                  axis.line.x.bottom = element_line(size=.5),
-                  axis.line.y.left = element_line(size=.5),
-                  legend.key = element_blank(),
-                  legend.text = element_text(size=13),
-                  panel.background = element_blank())
-
-
+#read in data for diet nutritional rails
 rails <- fread("Output/dietrails.rds")
 
 ggplot()+
