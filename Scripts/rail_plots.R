922a35941790873f1eebd964ded74ec7726c0592
Juliana Balluffi-Fry <jballuffi@gmail.com>
2022-06-03 16:01:56 -0600

    change variable names in plot

--- Scripts/rail_plots.R
+++ Scripts/rail_plots.R
@@ -8,16 +8,15 @@ lapply(dir('R', '*.R', full.names = TRUE), source)
 rails <- fread("Output/dietrails.rds")
 
 #read in single choice results
-single <- readRDS("Output/trialresultscleaned.rds")
+trials <- readRDS("Output/trialresultscleaned.rds")
 
 #plot showing individual responses
 (feedingratesrails<-
     ggplot()+
     geom_line(aes(x = NDF_IR, y = CP_IR, group = Diet), color = "black", data = rails)+
-    geom_point(aes(x = Consumed_NDF, y = Consumed_CP, color = Weight_change), size = 4, alpha = .7, data = SCdiets)+
+    geom_point(aes(x = NDF_in_bw, y = CP_in_bw, color = Weight_change), size = 4, alpha = .7, data = trials)+
     scale_color_gradient(low = "yellow2", high = "Blue3", name = "Weight change (%/day)")+
-    geom_point(aes(x = mean(NDF), mean(CP)), size = 3, shape = 10, data = multi)+
-    labs(x="Fibre intake (g/kg/day)", y="Protein intake (g/kg/day)")+
+    labs(x="NDF intake (g DM/kg/day)", y="CP intake (g DM/kg/day)")+
     themerails)
 
 
