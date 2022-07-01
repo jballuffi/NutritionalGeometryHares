4d47119ca4eb45b5eadfdb36884e724285165265
Juliana Balluffi-Fry <jballuffi@gmail.com>
2022-07-01 12:32:40 -0600

    add new theme

--- R/ggplot_themes.R
+++ R/ggplot_themes.R
@@ -12,3 +12,17 @@ themerails<-theme(axis.title = element_text(size=13),
                   legend.key = element_blank(),
                   legend.text = element_text(size=13),
                   panel.background = element_blank())
+
+
+themepoints <- theme(axis.title = element_text(size=13),
+                        axis.text = element_text(size=10),
+                        legend.position = "top",
+                        legend.key = element_blank(),
+                        legend.title = element_blank(),
+                        panel.background = element_blank(),
+                        axis.line.x.top = element_blank(),
+                        axis.line.y.right = element_blank(),
+                        axis.line.x.bottom = element_line(size=.5),
+                        axis.line.y.left = element_line(size=.5),
+                        panel.border = element_blank(),
+                        panel.grid.major = element_line(size = 0.5, color = "grey90"))
