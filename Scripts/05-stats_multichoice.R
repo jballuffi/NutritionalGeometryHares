831dc03c26c6da28126ca29f2ff7c9f3846b4c50
Juliana Balluffi-Fry <jballuffi@gmail.com>
2022-07-07 15:20:43 -0600

    add mean values

--- Scripts/05-stats_multichoice.R
+++ Scripts/05-stats_multichoice.R
@@ -7,24 +7,27 @@ MC <- readRDS("Output/data/multichoiceresults.rds")
 #read in multichoice trial sums
 sums <- readRDS("Output/data/multichoicesums.rds")
 
-#mean intake rates
+#mean intake rate across all diets
 meanNI <- round(mean(sums$Intake), digits = 2)
 sdNI <- round(sd(sums$Intake), digits = 2)
 
+# #mean intake rates by diet
+# NIA <- MC[Diet == "A", round(mean(Intake_bw), digits = 2)]
+# NIB <- MC[Diet == "B", round(mean(Intake_bw), digits = 2)]
+# NIC <- MC[Diet == "C", round(mean(Intake_bw), digits = 2)]
+# NID <- MC[Diet == "D", round(mean(Intake_bw), digits = 2)]
 
-#mean intake rates of CP and NDF during naive trials
+#mean intake rates of CP and NDF when summing all diets
 meanNCP <- round(mean(sums$CP), digits = 2)
 sdNCP <- round(sd(sums$CP), digits = 2)
 meanNNDF <- round(mean(sums$NDF), digits = 2)
 sdNNDF <- round(sd(sums$NDF), digits = 2)
 
-#collect mean intake rates by diet for naive hares
-NIdient <- MC[, mean(Intake_bw), Diet]
-
 
 #ANOVA testing for significance between treatments
 lmMC <- lm(MC$Intake_bw ~ MC$Diet)
 aMC <- anova(lmMC)
-aMC$`Pr(>F)`[1]
+#setnames(aMC, "Pr(>F)", "pval", skip_absent=TRUE)
+aMCpval <- round(aMC$`Pr(>F)`[1], 2)
 
 
