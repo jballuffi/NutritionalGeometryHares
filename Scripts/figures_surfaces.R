954c1bc87939ba7a2579742554602f4c126378b5
Juliana Balluffi-Fry <jballuffi@gmail.com>
2022-07-26 11:07:09 -0600

    surface figure testing

--- Scripts/figures_surfaces.R
+++ Scripts/figures_surfaces.R
@@ -11,6 +11,10 @@ day <- readRDS("Output/data/dailyresultscleaned.rds")
 
 #example of quiltplot
 data( ozone2)
+fitozone <- Tps(ozone2$lon.lat, ozone2$y[16,], scale.type = "range")
+surface(fitozone)
+image.plot(fitozone)
+
 quilt.plot( ozone2$lon.lat, ozone2$y[16,])
 
 
@@ -22,6 +26,8 @@ quilt.plot( ozone2$lon.lat, ozone2$y[16,])
 fitweight <- Tps(trials[, .(NDF_in_bw, CP_in_bw)], trials$Weight_change, scale.type = "range")
 surface(fitweight)
 
+
+
 fitdigCP <- Tps(day[, .(NDF_in_bw, CP_in_bw)], day$CP_dig, scale.type = "range")
 surface(fitdigCP)
 
@@ -30,6 +36,12 @@ surface(fitdigNDF)
 
 fitdigADF <- Tps(day[, .(NDF_in_bw, CP_in_bw)], day$ADF_dig, scale.type = "range")
 surface(fitdigADF)
+
+plot(fitdigCP)
+
+ggplot(fitdigCP)+
+  geom_raster(aes(x = x, y = y, fill = z), interpolate = FALSE)
+  
 
 
 savePlot("Output/figures/CPsurface.jpeg", CPdig, type = "jpg")
