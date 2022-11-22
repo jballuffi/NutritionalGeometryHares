8d4f0ee26aa90515381488534f264c038311b92e
Juliana Balluffi-Fry <jballuffi@gmail.com>
2022-11-21 23:45:09 -0700

    intake by weight

--- R/function_trialavg.R
+++ R/function_trialavg.R
@@ -9,7 +9,8 @@ trialavg <- function(dt){
   cols <- c("CP_diet", "NDF_diet", "ADF_diet", "ADL_diet",
             "DMI", "DMI_CP", "DMI_NDF", "DMI_ADF", "DMI_ADL", 
             "Weight_start", "Weight_end", #weights are the same for every day because we only took a measure at the start and end of trital
-            "DMF", "DP", "DNDF", "DADF", "DADL", 
+            "DMI_bw", "DMI_CP_bw", "DMI_NDF_bw", "DMI_ADF_bw", "DMI_ADL_bw", 
+            "DMD", "DMF", "DP", "DNDF", "DADF", "DADL", 
             "Temp")
   out <- dt[, lapply(.SD, mean), .SDcols = cols]
   return(out)
