bf02d197e596e547cced9c8d36cba7f8a3cd60ea
Juliana Balluffi-Fry <jballuffi@gmail.com>
2022-11-22 15:50:53 -0700

    update var names

--- Scripts/figures_excretion.R
+++ Scripts/figures_excretion.R
@@ -14,29 +14,29 @@ days[, Weight_start := Weight_start/1000]
 
 # Nutrient intake and excretion rates ----------------------------------------------
 
-total <- 
+(total <- 
   ggplot(days)+
-  geom_point(aes(x = Intake, y = Total_out, color = Diet))+
+  geom_point(aes(x = DMI, y = DMF, color = Diet))+
   labs(x = "Intake Rate (g DM/day)", y = "Excretion Rate (g DM/day")+
-  themepoints
+  themepoints)
 
-protein <-
+(protein <-
   ggplot(days)+
-  geom_point(aes(x = CP_in, y = CP_out, color = Diet))+
+  geom_point(aes(x = DMI_CP, y = DMF_CP, color = Diet))+
   labs(x = "Protein Intake (g DM/day)", y = "Protein Excretion (g DM/day")+
-  themepoints
+  themepoints)
 
-NDF <- 
+(NDF <- 
   ggplot(days)+
-  geom_point(aes(x = NDF_in_bw, y = NDF_out, color = Diet))+
+  geom_point(aes(x = DMI_NDF, y = DMF_NDF, color = Diet))+
   labs(x = "NDF Intake (g DM/day)", y = "NDF Excretion (g DM/day")+
-  themepoints
+  themepoints)
 
-ADF <- 
+(ADF <- 
   ggplot(days)+
-  geom_point(aes(x = ADF_in, ADF_out, color = Diet))+
+  geom_point(aes(x = DMI_ADF, y = DMF_ADF, color = Diet))+
   labs(x = "ADF Intake (g DM/day", y = "ADF Excretion (g DM/day)")+
-  themepoints
+  themepoints)
 
 fullfig <- ggarrange(total, protein, NDF, ADF, ncol = 2, nrow = 2)
 
