2166d2966e6b6535df886b0e3795499018ebc5d3
Juliana Balluffi-Fry <jballuffi@gmail.com>
2022-11-21 23:40:44 -0700

    rename cols and create new calculations

--- Scripts/02-multichoice_results_prep.R
+++ Scripts/02-multichoice_results_prep.R
@@ -21,8 +21,8 @@ diets <- fread("Input/Diet_compositions.csv")
 # merge data together -----------------------------------------------------
 
 #cut diet compositions to just be DM
-dietDM <- diets[, .(mean(DM, na.rm = TRUE), mean(CP_DM/100, na.rm = TRUE), mean(NDF_DM/100, na.rm = TRUE), mean(ADF_DM/100, na.rm = TRUE), mean(ADL_DM/100, na.rm = TRUE), mean(C_DM/100, na.rm = TRUE)), Sample]
-names(dietDM) <- c("Diet", "DM_diet", "CP_diet", "NDF_diet", "ADF_diet", "ADL_diet", "C_diet") #C isnt predicted it was measured after (not for paper)
+dietDM <- diets[, .(mean(DM, na.rm = TRUE), mean(CP_diet/100, na.rm = TRUE), mean(NDF_diet/100, na.rm = TRUE), mean(ADF_diet/100, na.rm = TRUE), mean(ADL_diet/100, na.rm = TRUE), mean(C_diet/100, na.rm = TRUE)), Sample]
+names(dietDM) <- c("Diet", "DM_diet", "CP_diet", "NDF_diet", "ADF_diet", "ADL_diet", "C_diet" )
 
 #subset DM data
 DM <- DM[, .(Sample, DM)]
@@ -53,16 +53,16 @@ DT[, OfferDM := Offer_wet*DM_diet]
 DT[, EndDM := End_wet*(DM/100)]
 
 #calculate daily intake rate in DM
-DT[, Intake := OfferDM - EndDM]
+DT[, DMI := OfferDM - EndDM]
 #if intake is neg make zero 
-DT[Intake < 0, Intake := 0]
+DT[DMI < 0, DMI := 0]
 
 #calculate intake rates of each nutrient
-DT[, CP_in := Intake*CP_diet]
-DT[, NDF_in := Intake*NDF_diet]
-DT[, ADF_in := Intake*ADF_diet]
-DT[, ADL_in := Intake*ADL_diet]
-DT[, C_in := Intake*C_diet]
+DT[, DMI_CP := DMI*CP_diet]
+DT[, DMI_NDF := DMI*NDF_diet]
+DT[, DMI_ADF := DMI*ADF_diet]
+DT[, DMI_ADL := DMI*ADL_diet]
+DT[, DMI_C := DMI*C_diet]
 
 
 
@@ -72,12 +72,13 @@ DT[, C_in := Intake*C_diet]
 #calculate intake rates by weight
 DT[, Weight_start := Weight_start/1000]
 
-DT[, Intake_bw := Intake/Weight_start]
-DT[, CP_in_bw := CP_in/Weight_start]
-DT[, NDF_in_bw := NDF_in/Weight_start]
-DT[, ADF_in_bw := ADF_in/Weight_start]
-DT[, ADL_in_bw := ADF_in/Weight_start]
-DT[, C_in_bw:= C_in/Weight_start]
+#calculate dry matter intakes by kg^.75
+DT[, DMI_bw := DMI/(Weight_start^.75)]
+DT[, DMI_CP_bw := DMI_CP/(Weight_start^.75)]
+DT[, DMI_NDF_bw := DMI_NDF/(Weight_start^.75)]
+DT[, DMI_ADF_bw := DMI_ADF/(Weight_start^.75)]
+DT[, DMI_ADF_bw := DMI_ADL/(Weight_start^.75)]
+DT[, DMI_C_bw := DMI_C/(Weight_start^.75)]
 
 
 
@@ -85,9 +86,9 @@ DT[, C_in_bw:= C_in/Weight_start]
 # Sum nutrient intakes by individual------------------------------------------------------------
 
 #calculate total protein and fibre consumed from all diets in one day
-totals <- DT[, .(sum(Intake_bw), sum(CP_in_bw), sum(NDF_in_bw)), by = ID]
+totals <- DT[, .(sum(DMI_bw), sum(DMI_CP_bw), sum(DMI_NDF_bw)), by = ID]
 
-names(totals) <- c("ID","Intake", "CP", "NDF")
+names(totals) <- c("ID","DMI_bw", "DMI_CP_bw", "DMI_NDF_bw")
 
 
 
