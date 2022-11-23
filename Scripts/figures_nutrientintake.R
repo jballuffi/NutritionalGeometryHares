89a1e6b63526526d416494f7adbaf3d9b9d8a89b
Juliana Balluffi-Fry <jballuffi@gmail.com>
2022-11-22 22:29:30 -0700

    weight change in response to digestible intake

--- Scripts/figures_nutrientintake.R
+++ Scripts/figures_nutrientintake.R
@@ -13,20 +13,72 @@ days <- readRDS("Output/data/dailyresultscleaned.rds")
 
 
 
-# wieght change in response to nutrient intake ----------------------------
+# weight change in response to crude matter intake ----------------------------
 
+(DMintake <- 
+  ggplot(trials)+
+  geom_point(aes(x = DMI_bw, y = Weight_change, color = Diet), size = 1.75)+
+  labs(y = "Weight change (%/Day)", x = "Dry Matter Intake (g/kg^0.75/day)")+
+  #scale_x_continuous("\n Protein Intake (g DM/k^0.75/day) \n", n.breaks =5) +
+  themerails)
 
 (proteinintake <-
    ggplot(trials)+
-   geom_point(aes(x = CP_in_bw, y = Weight_change), size = 1.75, color = "grey20")+
-   labs(y = "Weight change (%/Day)", x = "Protein intake (g DM/kg/day)")+
+   geom_point(aes(x = DMI_CP_bw, y = Weight_change, color = Diet), size = 1.75)+
+   labs(y = "Weight change (%/Day)", x = "Protein intake (g DM/kg^0.75/day)")+
+   #scale_x_continuous("\n Protein Intake (g DM/k^0.75/day) \n", n.breaks =5) +
    themerails)
 
 (NDFintake <-
     ggplot(trials)+
-    geom_point(aes(x = NDF_in_bw, y = Weight_change), size = 1.75, color = "grey20")+
-    labs(y = "Weight change (%/Day)", x = "NDF intake (g DM/kg/day)")+
+    geom_point(aes(x = DMI_NDF_bw, y = Weight_change, color = Diet), size = 1.75)+
+    labs(y = "Weight change (%/Day)", x = "NDF intake (g DM/kg^0.75/day)")+
     themerails)
+
+
+
+# Weight change in response to digestible matter intake -------------------
+
+
+(DMDintake <- 
+   ggplot(trials)+
+   geom_point(aes(x = DMDI, y = Weight_change, color = Diet), size = 1.75)+
+   labs(y = "Weight change (%/Day)", x = "Dry Matter Digestibility Intake (g/kg^0.75/day)")+
+   #scale_x_continuous("\n Protein Intake (g DM/k^0.75/day) \n", n.breaks =5) +
+   themerails)
+
+(digproteinintake <-
+    ggplot(trials)+
+    geom_point(aes(x = DPI, y = Weight_change, color = Diet), size = 1.75)+
+    labs(y = "Weight change (%/Day)", x = "Digestible Protein intake (g DM/kg^0.75/day)")+
+    #scale_x_continuous("\n Protein Intake (g DM/k^0.75/day) \n", n.breaks =5) +
+    themerails)
+
+(digNDFintake <-
+    ggplot(trials)+
+    geom_point(aes(x = DNDFI, y = Weight_change, color = Diet), size = 1.75,)+
+    labs(y = "Weight change (%/Day)", x = "Digestible NDF Intake (g DM/kg^0.75/day)")+
+    themerails)
+
+
+
+# Digestable intake vs. regular intake ------------------------------------
+
+ggplot(days)+
+  geom_point(aes(x = DMI_bw, y = DMDI, color = Diet), size = 1.75)+
+  labs(x = "Dry matter intake (g/kg^0.75/day)", y = "Dry matter digestibility intake (g/kg^0.75/day)")+
+  themerails
+
+ggplot(days)+
+  geom_point(aes(x = DMI_CP_bw, y = DPI, color = Diet), size = 1.75)+
+  labs(y = "Digestible protein intake (g/kg^0.75/day", x = "Protein intake (g/kg^0.75/day")+
+  themerails
+
+ggplot(days)+
+  geom_point(aes(x = DMI_NDF_bw, y = DNDFI, color = Diet), size = 1.75)+
+  labs(y = "Digestible NDF intake (g/kg^0.75/day", x = "NDF intake (g/kg^0.75/day")+
+  themerails
+
 
 
 
