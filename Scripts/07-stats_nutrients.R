57be2b0c65838717060802c2ee86d438ab70cce7
Juliana Balluffi-Fry <38500356+jballuffi@users.noreply.github.com>
2023-08-02 12:12:21 -0600

    remove models not needed

--- Scripts/07-stats_nutrients.R
+++ Scripts/07-stats_nutrients.R
@@ -12,67 +12,48 @@ day<- readRDS("Output/data/dailyresultscleaned.rds")
 
 # Weight change figures -----------------------------------------------------------
 
-test <- lm(Weight_change ~ DMI_CP_bw + DMI_NDF_bw + 0, trials)
-summary(test)
-
+#regular NDF intake
 ggplot(trials)+
   geom_point(aes(x = DMI_NDF_bw, y = Weight_change))+
-  geom_abline(intercept = 0, slope = -0.021005)+
-  xlim(0, 80)
+  theme_minimal()
 
+#digestible NDF intake
+ggplot(trials)+
+  geom_point(aes(x = DNDFI, y = Weight_change))+
+  theme_minimal()
+
+#regular protein intake
 ggplot(trials)+
   geom_point(aes(x = DMI_CP_bw, y = Weight_change))+
-  geom_abline(intercept = 0, slope = 0.034366)+
-  xlim(0, 17)
+  theme_minimal()
 
-
-
-# models for non-digestible nutrients -------------------------------------
-
-ND1 <- gam(Weight_change ~ DMI_CP_bw + DMI_NDF_bw, data = trials)
-ND2 <- gam(Weight_change ~ DMI_CP_bw*DMI_NDF_bw, data = trials)
-ND3 <- gam(Weight_change ~ s(DMI_CP_bw) + s(DMI_NDF_bw), data = trials)
-ND4 <- gam(Weight_change ~ s(DMI_CP_bw, DMI_NDF_bw), data = trials)
-ND5 <- gam(Weight_change ~ s(DMI_CP_bw, by = DMI_NDF_bw), data = trials)
-ND6 <- gam(Weight_change ~ s(DMI_NDF_bw, by = DMI_CP_bw), data = trials)
-
-
-
-# models for digestible nutrient intake -----------------------------------
-
-D1 <- gam(Weight_change ~ DPI + DNDFI, data = trials)
-D2 <- gam(Weight_change ~ DPI*DNDFI, data = trials)
-D3 <- gam(Weight_change ~ s(DPI) + s(DNDFI), data = trials)
-D4 <- gam(Weight_change ~ s(DPI, DNDFI), data = trials)
-D5 <- gam(Weight_change ~ s(DPI, by = DNDFI), data = trials)
-D6 <- gam(Weight_change ~ s(DNDFI, by = DPI), data = trials)
-
-
-
-# basic linear models for digestability effects on weight change -----------------------------------
-
-summary(lm(Weight_change ~ DMDI, trials))
-summary(lm(Weight_change ~ DMI, trials))
+#digestible protein intake
+ggplot(trials)+
+  geom_point(aes(x = DPI, y = Weight_change))+
+  theme_minimal()
 
 
 
 
-# poly models -------------------------------------------------------------
+# models for non-digestible intake on weight change -------------------------------------
 
-int_lm <- lm(Weight_change ~ DMI_CP_bw*DMI_NDF_bw, trials)
-int_poly <- lm(Weight_change ~ poly(DMI_CP_bw, 2)*poly(DMI_NDF_bw, 2), trials)
-lmm <- lm(Weight_change ~ DMI_CP_bw + DMI_NDF_bw, trials)
-polym <- lm(Weight_change ~ poly(DMI_CP_bw, 2) + poly(DMI_NDF_bw, 2), trials)
-
-
-Mods <- list(int_lm, int_poly, lmm, polym)
-names <- c("int_lm", "int_poly", "lmm", "polym")
-
-AIC<-as.data.table(aictab(REML=F, cand.set = Mods, modnames = names, sort = TRUE))
-AIC[, ModelLik := NULL]
-AIC[, Cum.Wt := NULL]
-#round whole table to 3 dec places
-AIC <- AIC %>% mutate_if(is.numeric, round, digits=3)
+bodyNDlm <- gam(Weight_change ~ DMI_NDF_bw*DMI_CP_bw, data = trials)
+BodyNDgam <- gam(Weight_change ~ s(DMI_NDF_bw, DMI_CP_bw), data = trials)
 
 
 
+# models for digestible intake on weight change -----------------------------------
+
+bodyDlm <- gam(Weight_change ~ DNDFI*DPI, data = trials)
+bodyDgam <- gam(Weight_change ~ s(DNDFI, DPI), data = trials)
+
+
+
+#  models for intake on DMD -----------------------------------------------
+
+DMDlm <- gam(DMD ~ DMI_NDF_bw*DMI_CP_bw, data = day)
+DMDgam <- gam(DMD ~ s(DMI_NDF_bw, DMI_CP_bw), data = day)
+
+
+
+        
