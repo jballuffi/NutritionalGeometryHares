54278c2f35c8281870da238616a1422df0b9b755
Juliana Balluffi-Fry <38500356+jballuffi@users.noreply.github.com>
2023-08-02 16:49:26 -0600

    makes gams and save summaries of gams to csv table

--- Scripts/07-stats_nutrients.R
+++ Scripts/07-stats_nutrients.R
@@ -9,8 +9,86 @@ day<- readRDS("Output/data/dailyresultscleaned.rds")
 
 
 
+# model for non-digestible intake on weight change -------------------------------------
 
-# Weight change figures -----------------------------------------------------------
+bodyND <- gam(Weight_change ~ s(DMI_NDF_bw, DMI_CP_bw), data = trials)
+
+#save summary of model
+sum <- summary(bodyND)
+  
+#make p-table, and indicate model, this is for intercept stuff
+sump <- as.data.table(round((sum$p.table), 4))
+sump[, Model := "Crude macronutrient intake"]
+sump[, `Dev. Explained` := round(sum$dev.expl, 2)]
+  
+#make s-table, and indicate model, this is for variable stuff
+sums <- as.data.table(round((sum$s.table), 4))
+sums[, Model := "Crude macronutrient intake"]
+
+ 
+
+# model for digestible intake on weight change -----------------------------------
+
+bodyD <- gam(Weight_change ~ s(DNDFI, DPI), data = trials)
+
+#save summary of model
+sum2 <- summary(bodyD)
+
+#make p-table, and indicate model, this is for intercept stuff
+sum2p <- as.data.table(round((sum2$p.table), 4))
+sum2p[, Model := "Digestible macro-nutrient intake"]
+sum2p[, `Dev. Explained` := round(sum2$dev.expl, 2)]
+
+#make s-table, and indicate model, this is for variable stuff
+sum2s <- as.data.table(round((sum2$s.table), 4))
+sum2s[, Model := "Digestible macro-nutrient intake"]
+
+
+#  model for intake on DMD -----------------------------------------------
+
+DMD <- gam(DMD ~ s(DMI_NDF_bw, DMI_CP_bw), data = day)
+
+#save summary of model
+sum3 <- summary(DMD)
+
+#make p-table, intercept stuff
+sum3p <- as.data.table(round((sum3$p.table), 4))
+sum3p[, Model := "DMD"]
+sum3p[, `Dev. Explained` := round(sum3$dev.expl, 2)]
+
+#make s-table, intercept stuff
+sum3s <- as.data.table(round((sum3$s.table), 4))
+sum3s[, Model := "DMD"]
+
+
+
+# Merge output tables -----------------------------------------------------
+
+#bind all p tables
+allsump <- rbind(sump, sum2p, sum3p)
+names(allsump) <- c("Estimate/edf", "SE/Ref.df", "t/F", "p", "Model", "Dev. Explained")
+allsump[, Parameter := "Intercept"]
+
+#bind all s tables
+allsums <- rbind(sums, sum2s, sum3s)
+names(allsums) <- c("Estimate/edf", "SE/Ref.df", "t/F", "p", "Model")
+allsums[, Parameter := "s(DMI_NDF, DMI_protein)"]
+
+#bind p and s tables together
+summarytable <- rbind(allsump, allsums, fill = TRUE)
+setorder(summarytable, Model)
+setcolorder(summarytable, c("Model", "Parameter", "Dev. Explained", "Estimate/edf", "SE/Ref.df", "t/F", "p"))
+
+
+
+# Save table --------------------------------------------------------------
+
+write.csv(summarytable, "Output/GAMoutputs.csv")
+
+
+
+
+# Figures -----------------------------------------------------------
 
 #regular NDF intake
 ggplot(trials)+
@@ -32,28 +110,15 @@ ggplot(trials)+
   geom_point(aes(x = DPI, y = Weight_change))+
   theme_minimal()
 
+#protein intake to DMD
+ggplot(day)+
+  geom_point(aes(x = DMI_CP_bw, y = DMD))+
+  theme_minimal()
 
-
-
-# models for non-digestible intake on weight change -------------------------------------
-
-bodyNDlm <- gam(Weight_change ~ DMI_NDF_bw*DMI_CP_bw, data = trials)
-BodyNDgam <- gam(Weight_change ~ s(DMI_NDF_bw, DMI_CP_bw), data = trials)
-
-
-
-# models for digestible intake on weight change -----------------------------------
-
-bodyDlm <- gam(Weight_change ~ DNDFI*DPI, data = trials)
-bodyDgam <- gam(Weight_change ~ s(DNDFI, DPI), data = trials)
-
-
-
-#  models for intake on DMD -----------------------------------------------
-
-DMDlm <- gam(DMD ~ DMI_NDF_bw*DMI_CP_bw, data = day)
-DMDgam <- gam(DMD ~ s(DMI_NDF_bw, DMI_CP_bw), data = day)
-
+#NDF intake to DMD
+ggplot(day)+
+  geom_point(aes(x = DMI_NDF_bw, y = DMD))+
+  theme_minimal()
 
 
         
