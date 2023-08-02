226ede4c3cc30c077cca641401a9c337e1be727e
Juliana Balluffi-Fry <38500356+jballuffi@users.noreply.github.com>
2023-08-02 15:39:40 -0600

    change axis titles

--- Scripts/figures_surfaces.R
+++ Scripts/figures_surfaces.R
@@ -13,17 +13,17 @@ day <- readRDS("Output/data/dailyresultscleaned.rds")
 #not accounting for digestibility
 fitCP <- Tps(trials[, .(DMI_NDF_bw, DMI_CP_bw)], trials$Weight_change, scale.type = "range")
 surface(fitCP, x = "NDF intake (g DM/kg^0.75/day)", 
-        y = "CP intake (g DM/kg^0.75/day)")
+        y = "Protein intake (g DM/kg^0.75/day)")
 
 #yes accounting for digestibility
 fitDP <- Tps(trials[, .(DNDFI, DPI)], trials$Weight_change, scale.type = "range")
 surface(fitDP, x = "Digestible NDF intake (g DM/kg^0.75/day)", 
-        y = "Digestible CP intake (g DM/kg^0.75/day)")
+        y = "Digestible protein intake (g DM/kg^0.75/day)")
 
 #NDF and CP intake effect on dry matter digestibility
 dmd <- Tps(trials[, .(DMI_NDF_bw, DMI_CP_bw)], trials$DMD, scale.type = "range")
 surface(dmd, x = "NDF intake (g DM/kg^0.75/day)", 
-        y = "CP intake (g DM/kg^0.75/day)")
+        y = "Protein intake (g DM/kg^0.75/day)")
 
 
 
