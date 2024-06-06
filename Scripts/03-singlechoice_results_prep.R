db60c52646cede8f5c0d3189d5c4671c45911c6f
Juliana Balluffi-Fry <38500356+jballuffi@users.noreply.github.com>
2024-06-06 14:15:24 -0600

    add energy to output, fixed digestible calc error

--- Scripts/03-singlechoice_results_prep.R
+++ Scripts/03-singlechoice_results_prep.R
@@ -239,14 +239,14 @@ DT[, Temp := tempcalc(start = DayTime_start, end = DayTime_end), by = .(ID, Tria
 
 #cut out a datasheet of just key feeding trial info and results
 Dailyresults <- DT[, c("Diet", "Sample", "ID", "Trial", "Day", "Date_start", "Date_end", "Date", #info
-                       "CP_diet", "NDF_diet", "ADF_diet", "ADL_diet", #diet compositions
-                       "DMI", "DMI_CP", "DMI_NDF", "DMI_ADF", "DMI_ADL", #dry matter intakes (g/day)
+                       "CP_diet", "NDF_diet", "ADF_diet", "ADL_diet", "Energy_diet", #diet compositions
+                       "DMI", "DMI_CP", "DMI_NDF", "DMI_ADF", "DMI_ADL", "DMI_energy", #dry matter intakes (g/day)
                        "Weight_start", "Weight_end", #weights (g)
-                       "DMI_bw", "DMI_CP_bw", "DMI_NDF_bw", "DMI_ADF_bw", "DMI_ADL_bw", #dry matter intake by weight (g/kg^.75/day)
+                       "DMI_bw", "DMI_CP_bw", "DMI_NDF_bw", "DMI_ADF_bw", "DMI_ADL_bw", "DMI_energy_bw", #dry matter intake by weight (g/kg^.75/day)
                        "DMF", "DMF_CP", "DMF_NDF", "DMF_ADF", "DMF_ADL", #fecal outputs (g/day) 
                        "CP_F", "NDF_F", "ADF_F", "ADL_F", #fecal compositions (%)
-                       "DMD", "DP", "DNDF", "DADF", "DADL",  #digestibility (%)
-                       "DMDI", "DPI", "DNDFI", "DADFI", #digestible intake rate (g/kg^.75/day)
+                       "DMD", "DP", "DNDF", "DADF", "DADL", "DE",  #digestibility (%)
+                       "DMDI", "DPI", "DNDFI", "DADFI", "DEI", #digestible intake rate (g/kg^.75/day)
                        "Temp"
 )] 
 
