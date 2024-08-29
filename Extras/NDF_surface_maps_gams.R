3af760315b037aad947ad07dfdc9902539b23ecc
Juliana Balluffi-Fry <38500356+jballuffi@users.noreply.github.com>
2024-08-29 14:52:24 -0600

    move NDF surface maps

--- Extras/NDF_surface_maps_gams.R
+++ Extras/NDF_surface_maps_gams.R
@@ -14,6 +14,7 @@ rails <- fread("Output/data/dietdigestionrails.rds")
 MCsums <- readRDS("Output/data/multichoicesums.rds")
 
 
+
 # model weight change ~ crude NDF -------------------------------------
 
 #bodyCNDF <- gam(Weight_change ~ s(DMI_NDF_bw, DMI_CP_bw), data = trials)
@@ -50,4 +51,36 @@ sumpDNDF[, `Dev. Explained` := round(sumDNDF$dev.expl, 2)]
 #make s-table, and indicate model, this is for variable stuff
 sumsDNDF <- as.data.table(round((sumDNDF$s.table), 4))
 sumsDNDF[, Model := "Digestible NDF"]
+
+
+
+#extas
+(c <- ggplot()+
+    geom_raster(aes(x = DMI_NDF_bw, y = DMI_CP_bw, z = fit, fill = fit), data = CNDF)+
+    geom_contour(aes(x = DMI_NDF_bw, y = DMI_CP_bw, z = fit), bins = 5, colour = "grey90", data = CNDF)+
+    scale_fill_continuous(name = "%/day", type = "viridis")+
+    geom_line(aes(x = NDF_IR, y = CP_IR, group = Diet, linetype = Diet), size = .8, data = rails)+
+    scale_linetype_manual(values = dietlines, guide = NULL)+
+    geom_point(aes(x = meanNDFI, y = meanCPI), data = targets)+
+    xlim(min(CNDF$DMI_NDF_bw), max(CNDF$DMI_NDF_bw))+
+    ylim(min(CNDF$DMI_CP_bw), max(CNDF$DMI_CP_bw))+
+    xlab(expression(NDF~intake~(g/kg^0.75/day)))+
+    ylab(expression(CP~intake~(g/kg^0.75/day)))+
+    labs(title = "A) Crude NDF and protein")+
+    themerails)
+
+d <- ggplot()+
+  geom_raster(aes(x = DNDFI, y = DPI, z = fit, fill = fit), data = DNDF)+
+  geom_contour(aes(x = DNDFI, y = DPI, z = fit), bins = 5, colour = "grey90", data = DNDF)+
+  scale_fill_continuous(name = "%/day", type = "viridis")+
+  geom_line(aes(x = DNDF_IR, y = DP_IR, group = Diet, linetype = Diet), size = .8, data = rails, )+
+  scale_linetype_manual(values = dietlines, guide = NULL)+
+  geom_point(aes(x = meanDNDFI, y = meanDPI), data = targets)+
+  xlim(min(DNDF$DNDFI), max(DNDF$DNDFI))+
+  ylim(min(DNDF$DPI), max(DNDF$DPI))+
+  xlab(expression(DNDF~intake~(g/kg^0.75/day)))+
+  ylab(expression(DP~intake~(g/kg^0.75/day)))+
+  labs(title = "C) Digestible NDF and protein")+
+  themerails
+
 
