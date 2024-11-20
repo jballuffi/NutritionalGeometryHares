3c68523d8e9c3a04fa6850698deb80444db9722d
Juliana Balluffi-Fry <jballuffi@gmail.com>
2024-11-20 13:05:05 -0700

    add digestible intake to multichoice

--- Scripts/03-multichoice_results_prep.R
+++ Scripts/03-multichoice_results_prep.R
@@ -19,13 +19,14 @@ dietcomp <- fread("Output/data/dietcompositions.rds")
 
 diets <- fread("Input/Diet_compositions.csv")
 
+diet_digest <- readRDS("Output/data/diet_digestibilities.rds")
+
 
 
 # merge data together -----------------------------------------------------
 
 #remove diet DM column from dietcomp. Will be calculated for each winter
 dietcomp[, DM_diet := NULL]
-
 
 #average diet DM by winter
 dietDM <- diets[, .(DM_diet = mean(DM)), by = .(Winter, Sample)]
@@ -54,6 +55,7 @@ DT <- merge(DT, dietcomp, by = "Diet", all.x = TRUE)
 
 DT <- merge(DT, dietDM, by = c("Diet", "Winter"), all.x = TRUE)
 
+DT <- merge(DT, diet_digest, by = "Diet", all.x = TRUE)
 
 #missing DM gets mean DM
 avgSampleDM <- mean(DT$DM, na.rm =TRUE)
@@ -75,10 +77,7 @@ DT[DMI < 0, DMI := 0]
 #calculate intake rates of each nutrient
 DT[, DMI_CP := DMI*CP_diet]
 DT[, DMI_NDF := DMI*NDF_diet]
-DT[, DMI_ADF := DMI*ADF_diet]
-DT[, DMI_ADL := DMI*ADL_diet]
-DT[, DMI_C := DMI*C_diet]
-DT[, DMI_energy := DMI*Energy_diet]
+DT[, DMI_GE := DMI*Energy_diet]
 
 
 
@@ -91,11 +90,15 @@ DT[, Weight_start := Weight_start/1000]
 DT[, DMI_bw := DMI/(Weight_start^.75)]
 DT[, DMI_CP_bw := DMI_CP/(Weight_start^.75)]
 DT[, DMI_NDF_bw := DMI_NDF/(Weight_start^.75)]
-DT[, DMI_ADF_bw := DMI_ADF/(Weight_start^.75)]
-DT[, DMI_ADF_bw := DMI_ADL/(Weight_start^.75)]
-DT[, DMI_C_bw := DMI_C/(Weight_start^.75)]
-DT[, DMI_energy_bw := DMI_energy/(Weight_start^.75)]
+DT[, DMI_GE_bw := DMI_GE/(Weight_start^.75)]
 
+
+
+# Digestible intake rates by weight ---------------------------------------
+
+DT[, DMDI := DMD_diet*DMI_bw]        #digestible dry matter intake (g/kg.75)
+DT[, DPI := CPD_diet*DMI_CP_bw]       #digestible protein intake (g/kg.75)
+DT[, DEI := GED_diet*DMI_bw]              #digestible energy intake (kj/kg.75)
 
 
 # remove food strikers ----------------------------------------------------
