f30c28efe3314e754c7ac8485179749bb75c9a8b
Juliana Balluffi-Fry <jballuffi@gmail.com>
2024-11-20 14:43:28 -0700

    remove temp stuff

--- Scripts/02-singlechoice_results_prep.R
+++ Scripts/02-singlechoice_results_prep.R
@@ -24,10 +24,6 @@ spill <- fread("Input/Daily_food_remainders.csv")
 #read in fecal response data
 feces <- fread("Input/Results_feces.csv")
 
-#temp logger data
-temp2022 <- fread("Input/temperatures_SW_2022.csv")
-temp2023 <- fread("Input/temperature_SW_2023.csv")
-
 
 
 # Melt feeding trial data into individual days-------------------------------------------------
@@ -218,29 +214,6 @@ DT[, DEI := DE*DMI_bw] #digestible energy intake (kj/kg.75)
 
 
 
-# Merge in daily temperatures ---------------------------------------------
-
-#create a daily start and end time for feeding trials
-DT[, Time_start := "11:00:00"][, Time_end := "11:00:00"]
-
-#create a datetime for feeding trial starts and ends
-DT[, DayTime_start := lubridate::as_datetime(paste0(Date-1, " ", Time_start))]
-DT[, DayTime_end := lubridate::as_datetime(paste0(Date, " ", Time_end))]
-
-#get date columns from both years into the same format 
-temp2022[, Date := lubridate::ymd(Date)]
-temp2023[, Date := lubridate::mdy(Date)]
-
-#rbind both years of temperature data
-temp <- rbind(temp2022, temp2023)
-
-#in temp data, merge date and time into a datetime
-temp[, DateTime := lubridate::as_datetime(paste0(Date, " ", Time, " ", TimeStamp))]
-
-#run the tempcalc function (in R folder) by day and ID
-DT[, Temp := tempcalc(start = DayTime_start, end = DayTime_end), by = .(ID, Trial, Day)]
-
-
 
 # create simplified datasheet for daily results --------------------------------------
 
@@ -253,8 +226,8 @@ Dailyresults <- DT[, c("Diet", "Sample", "ID", "Trial", "Day", "Date_start", "Da
                        "DMF", "DMF_CP", "DMF_NDF", "DMF_ADF", "DMF_ADL", #fecal outputs (g/day) 
                        "CP_F", "NDF_F", "ADF_F", "ADL_F", #fecal compositions (%)
                        "DMD", "DP", "DNDF", "DADF", "DADL", "DE",  #digestibility (%)
-                       "DMDI", "DPI", "DNDFI", "DADFI", "DEI", #digestible intake rate (g/kg^.75/day)
-                       "Temp"
+                       "DMDI", "DPI", "DNDFI", "DADFI", "DEI" #digestible intake rate (g/kg^.75/day)
+                       
 )] 
 
 
