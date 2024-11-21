f68541680b8b02a93563ba2aca26d8a2d73b108d
Juliana Balluffi-Fry <38500356+jballuffi@users.noreply.github.com>
2024-11-21 10:56:16 -0700

    Revert "change acronyms in day vars"
    This reverts commit 4c09fa45d0321ee91a96825be6fef2dda7162448.

--- R/variables.R
+++ R/variables.R
@@ -1,13 +1,11 @@
 #list of column/variables that you want taken out of the cleaned sheet
 
-dayvars <- c("Diet", "Sample", "ID", "Trial", "Day", "Date_start", "Date_end", "Date", #info
-             "CP_diet", "NDF_diet", "ADF_diet", "ADL_diet", "GE_diet", #diet compositions
-             "DMI", "CPI", "NDFI", "GEI", #dry matter intakes (g/day)
+dayvars <- c("CP_diet", "NDF_diet", "ADF_diet", "ADL_diet", "Energy_diet", #diet compositions (%)
+             "DMI", "DMI_CP", "DMI_NDF", "DMI_ADF", "DMI_ADL", "DMI_energy", #dry matter intakes (g/day)
              "Weight_start", "Weight_end", #weights (g)
-             "DMI_bw", "CPI_bw", "NDFI_bw", "GEI_bw", #dry matter intake by weight (g/kg^.75/day)
-             "DMO", "CPO", "NDFO", #fecal outputs (g/day) 
-             "CP_F", "NDF_F", #fecal compositions (%)
-             "DMD", "CPD", "NDFD", "GED",  #digestibility (%)
-             "DMDI", "DPI", "DNDFI", "DEI") #digestible intake rate (g/kg^.75/day)
-             
-
+             "DMI_bw", "DMI_CP_bw", "DMI_NDF_bw", "DMI_ADF_bw", "DMI_ADL_bw", "DMI_energy_bw", #dry matter intake by weight (g/kg^.75/day)
+             "DMF", "DMF_CP", "DMF_NDF", "DMF_ADF", "DMF_ADL", #fecal outputs (g/day) 
+             "CP_F", "NDF_F", "ADF_F", "ADL_F", #fecal compositions (%)
+             "DMD", "DP", "DNDF", "DADF", "DADL", "DE",  #digestibility (%)
+             "DMDI", "DPI", "DNDFI", "DADFI", "DEI", #digestible intake rate (g/kg^.75/day)
+             "Temp") #temp (C)
