55586f71aebc0de8f197b2e6e0d911ab1a3368c1
Juliana Balluffi-Fry <38500356+jballuffi@users.noreply.github.com>
2024-11-22 13:19:25 -0700

    make master script

---
+++ Scripts/Master_script.R
@@ -0,0 +1,17 @@
+
+
+source("Scripts/01-make_nutritional_rails.R")
+
+source("Scripts/02-singlechoice_results_prep.R")
+
+source("Scripts/03-multichoice_results_prep.R")
+
+source("Scripts/04-digestible_rails_target_intake.R")
+
+source("Scripts/05-stats_treatment.R")
+
+source("Scripts/06-figures_treatment.R")
+
+source("Scripts/07-stats_surface.R")
+
+source("Scripts/08-stats_linear.R")
