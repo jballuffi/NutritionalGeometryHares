5d716e35ae15ffd9a3754086ac1b6d70b8b4589c
Juliana Balluffi-Fry <38500356+jballuffi@users.noreply.github.com>
2025-01-29 16:39:08 -0700

    spruce up fecal protein figures

--- Extras/figures_excretion.R
+++ Extras/figures_excretion.R
@@ -53,12 +53,12 @@ fullfig <- ggarrange(total, protein, NDF, ADF, ncol = 2, nrow = 2)
 # nutrient intake and excretion compositions (%) --------------------------
 
 
-(Proteincomp <- 
-  ggplot(days)+
-  geom_jitter(aes(x = CP_diet*100, y = CP_F), width = .5)+
-  geom_smooth(aes(x = CP_diet*100, y = CP_F))+
+
+Proteinintake <-  ggplot(days)+
+  geom_jitter(aes(x = CP_diet*100, y = CP_F), shape = 1, width = .5)+
+  geom_smooth(aes(x = CP_diet*100, y = CP_F), color = "black")+
   labs(x = "Diet crude protein (%)", y = "Fecal crude protein (%)")+
-  themerails)
+  themerails
 
 ggplot(trials)+
     geom_jitter(aes(x = CP_diet*100, y = Weight_change), width = .5)+
@@ -66,14 +66,16 @@ ggplot(trials)+
     labs(x = "Diet crude protein (%)", y = "Weight change (%)")+
     themerails
 
-ggplot(trials)+
-  geom_jitter(aes(x = CP_F, y = Weight_change), width = .5)+
-  geom_smooth(aes(x = CP_F, y = Weight_change), method = "lm")+
-  labs(x = "Diet crude protein (%)", y = "Weight change (%)")+
+Proteincomp <- ggplot(trials)+
+  geom_abline(intercept = 0, slope = 0, linetype = 2)+
+  geom_jitter(aes(x = CP_F, y = Weight_change), width = .5, shape = 1)+
+  geom_smooth(aes(x = CP_F, y = Weight_change), method = "lm", color = "black")+
+  labs(x = "Fecal crude protein (%)", y = "Weight change (%)")+
   themerails
 
-# save figures ------------------------------------------------------------
+# save figures ------------------------------------------------------------http://127.0.0.1:30311/graphics/63aa41fe-b6f4-4ee6-a023-5358019a1a6f.png
 
 
 ggsave("Output/figures/excretionrates.jpg", fullfig, width = 7, height = 6, units = "in")
 ggsave("Output/figures/fecalprotein.jpg", Proteincomp, width = 6, height = 5, units = "in")
+ggsave("Output/figures/fecalprotein_intake.jpg", Proteinintake, width = 6, height = 5, units = "in")
